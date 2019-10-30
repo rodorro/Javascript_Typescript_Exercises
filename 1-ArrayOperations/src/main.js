63af5bd8e6b2ae4b6f28879f8e14cdfe21b8ae8b
illano <illano@ecna.es>
2019-10-30 15:44:41 +0100

    First exercise init

--- 1-ArrayOperations/src/main.js
+++ 1-ArrayOperations/src/main.js
@@ -1,1 +1,24 @@
-console.log("This is your playground, experiment with JS code and check the console");
+const deportes = ["Baloncesto", "Tenis", "Fútbol", "Padel"];
+
+const head = ([head] = Array) => head;
+
+const tail = ([head, ...tail] = Array) => tail;
+
+const init = (array = Array) => {
+    Array.prototype.myUcase = function () {
+        for (i = 0; i < this.length; i++) {
+            this[i] = this[i].toUpperCase();
+        }
+    };
+    console.log(array.myUcase());
+}
+
+const last = () => {
+
+}
+
+console.log(head(deportes));
+
+console.log(tail(deportes));
+
+console.log(init(deportes));

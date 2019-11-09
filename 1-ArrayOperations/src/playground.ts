b02f04aacb04f3abc5c35f5693677a14e1b9fe68
Iban <rodorro@hotmail.com>
2019-11-09 13:09:47 +0100

    Ejercicio 1 - ArrayOperations

--- 1-ArrayOperations/src/playground.ts
+++ 1-ArrayOperations/src/playground.ts
@@ -11,6 +11,9 @@ const init = (array : Array<string>) => array.slice(0, array.length-1);
 const last = (array : Array<string>) => array[array.length-1]; 
 
 console.log(`El primer deporte de la lista es ${head(deportes)}`);
+
 console.log(`Todos los deportes menos el primero son ${tail(deportes)}`);
+
 console.log(`Todos los deportes menos el último son ${init(deportes)}`);
+
 console.log(`El último deporte de la lista es ${last(deportes)}`);

165b1b38bd1b0e38765622c4c6c47835af5861fe
Iban <rodorro@hotmail.com>
2019-11-09 12:42:13 +0100

    Ejercicio 3 - CloneMerge

--- 3-CloneMerge/src/playground.ts
+++ 3-CloneMerge/src/playground.ts
@@ -1,1 +1,44 @@
 console.log("Exercise 3: CloneMerge");
+
+const deportista = {
+    nombre: "Fernando",
+    apellido: "Belastegin",
+    edad: "40",
+    deporte: "Padel"
+}
+
+const persona = {
+    nombre: "Martina",
+    apellido: "Martin",
+    pais: "Argentina",
+}
+
+const clone = (source) => {
+
+    let cloned = {};
+
+    for(let prop in source) {
+        cloned[prop] = source[prop];
+    }
+
+    return cloned;
+}
+
+console.log("El deportista clonado es: ");
+console.log(clone(deportista));
+console.log("El deportista original es: ");
+console.log(deportista);
+
+const merge = (source, target) => {
+
+    let merged = {...target, ...source};
+
+    return merged;
+}
+
+console.log("El primer objeto es: ");
+console.log(deportista);
+console.log("El segundo objeto es: ");
+console.log(persona);
+console.log("El objeto mergeado es: ");
+console.log(merge(deportista, persona));
--- 4-ReadBooks/src/playground.ts
+++ 4-ReadBooks/src/playground.ts
@@ -1,1 +1,28 @@
 console.log("Exercise 4: ReadBooks");
+
+interface Book {
+    title: string;
+    isRead: boolean;
+}
+
+const libro1 = "Canción de hielo y fuego";
+const libro2 = "La rueda del tiempo";
+const libro3 = "Harry Potter y la piedra filosofal";
+const libro4 = "El código da Vinci";
+
+const books = [
+    {title: libro1, isRead: true},
+    {title: libro2, isRead: true},
+    {title: libro3, isRead: false},
+    {title: libro4, isRead: false},
+]
+
+const isBookRead = (books:Array<Book>, titleToSearch:string) : boolean => {
+
+    return books.find(book => book.title === titleToSearch).isRead;
+}
+
+console.log(`${libro1} ${isBookRead(books, libro1)?"leído":"no leído"}`);
+console.log(`${libro2} ${isBookRead(books, libro2)?"leído":"no leído"}`);
+console.log(`${libro3} ${isBookRead(books, libro3)?"leído":"no leído"}`);
+console.log(`${libro4} ${isBookRead(books, libro4)?"leído":"no leído"}`);

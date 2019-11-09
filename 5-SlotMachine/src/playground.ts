ae670a9695a47e44e05b3401d4b0b81d669cd329
Iban <rodorro@hotmail.com>
2019-11-09 13:08:39 +0100

    Ejercicio 5 - SlotMachine

--- 5-SlotMachine/src/playground.ts
+++ 5-SlotMachine/src/playground.ts
@@ -1,1 +1,39 @@
 console.log("Exercise 5: SlotMachine");
+
+class SlotMachine {
+
+    contMonedas = 0;
+
+    play = () => {
+
+        this.contMonedas++;
+
+        let ruleta1 = Math.random() >= 0.5;
+        let ruleta2 = Math.random() >= 0.5;
+        let ruleta3 = Math.random() >= 0.5;
+
+        let message = "";
+        if(ruleta1 === ruleta2 === ruleta3 === true) {
+            message = `Congratulations!!!. You won ${this.contMonedas} ${this.contMonedas>1?"coins":"coin"}`;
+            this.contMonedas = 0;
+        } else {
+            message = "Goog luck next time!!";
+        }
+
+        return message;
+    }
+}
+
+const machine1 = new SlotMachine();
+
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());
+console.log(machine1.play());

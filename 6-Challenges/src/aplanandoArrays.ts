2db24028813e535c7587ce7b6274419290c28d66
illano <illano@ecna.es>
2019-11-11 16:41:21 +0100

    Exercise 6 - Challenges

---
+++ 6-Challenges/src/aplanandoArrays.ts
@@ -0,0 +1,17 @@
+console.log("Aplanando Arrays");
+
+const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+
+interface NestedArray<T> extends Array<T | NestedArray<T>> {
+}
+
+//const flattenDeep = (array : NestedArray<number>) => array.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+
+//const flatDeep = (array : NestedArray<number>) => array.flat(Infinity);
+
+const flattenDeep = (array: NestedArray<number>) => array.reduce((acc : NestedArray<number>, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+
+console.log(flattenDeep(sample));
+
+//console.log(flatDeep(sample));
+

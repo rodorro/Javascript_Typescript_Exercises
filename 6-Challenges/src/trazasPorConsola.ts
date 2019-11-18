a71ddec0a4e6bbd395d9f7ce3061fa44e8779880
illano <illano@ecna.es>
2019-11-18 16:55:21 +0100

    Trazas por consola

---
+++ 6-Challenges/src/trazasPorConsola.ts
@@ -0,0 +1,16 @@
+console.log("Trazas por consola");
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const showMessage = async ([time, message]) => {
+    await delay(time);
+    console.log(message);
+};
+const triggers = [
+    async () => await showMessage([200, "third"]),
+    async () => await showMessage([100, "second"])
+];
+const run = triggers => {
+    console.log("first");
+    triggers.forEach(t => t());
+};
+run(triggers);

f57d502c71cdd3de206cb1baf7e68769f1bd1788
illano <illano@ecna.es>
2019-11-26 16:39:07 +0100

    Exercise 6 - Challengues

---
+++ 6-Challenges/src/estructuraArbol.ts
@@ -0,0 +1,13 @@
+console.log("Estructura de árbol tipada");
+
+interface TreeNode2<T> {
+  value: T;
+  children?: Array<TreeNode2<T>>;
+}
+
+console.log(`interface TreeNode < T > {
+        value: T;
+        children?: Array<TreeNode<T>>;
+    }`);
+
+console.log("***********************************************************");

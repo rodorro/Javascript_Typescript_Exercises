82c0b6e65f68d4c6cfcc8dd585c6447671cca3ac
illano <illano@ecna.es>
2019-11-26 16:41:54 +0100

    Exercise 6 - Challengues

--- 6-Challenges/src/trazasPorConsola.ts
+++ 6-Challenges/src/trazasPorConsola.ts
@@ -2,15 +2,16 @@ console.log("Trazas por consola");
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const showMessage = async ([time, message]) => {
-    await delay(time);
-    console.log(message);
+  await delay(time);
+  console.log(message);
 };
 const triggers = [
-    async () => await showMessage([200, "third"]),
-    async () => await showMessage([100, "second"])
+  async () => await showMessage([200, "third"]),
+  async () => await showMessage([100, "second"])
 ];
-const run = triggers => {
-    showMessage([300,"first"]);
-    triggers.forEach(t => t());
+const run = async triggers => {
+  await triggers[0]();
+  await triggers[1]();
+  console.log("first");
 };
 run(triggers);

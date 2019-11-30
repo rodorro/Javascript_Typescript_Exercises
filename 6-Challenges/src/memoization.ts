a23831822edc180fa2614ad168559cf4d1a86242
Iban <rodorro@hotmail.com>
2019-11-30 13:13:51 +0100

    Exercise 6: Challengues

---
+++ 6-Challenges/src/memoization.ts
@@ -0,0 +1,12 @@
+console.log("Challengue 5: Memoization");
+
+const expensiveFunction = () => {
+  console.log("Una única llamada");
+  return 3.1415;
+};
+
+//const memoize = ¿..?;
+//const memoized = memoize(expensiveFunction);
+//console.log(memoized()); // Una única llamada // 3.1415
+//console.log(memoized()); // 3.1415
+//console.log(memoized()); // 3.1415

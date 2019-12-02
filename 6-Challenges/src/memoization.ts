88fd9dcc8e6f0b7d8812bbd562278f87a3cf5fc6
illano <illano@ecna.es>
2019-12-02 08:38:55 +0100

    Exercise 6 - Challengues

--- 6-Challenges/src/memoization.ts
+++ 6-Challenges/src/memoization.ts
@@ -5,8 +5,16 @@ const expensiveFunction = () => {
   return 3.1415;
 };
 
-//const memoize = ¿..?;
-//const memoized = memoize(expensiveFunction);
-//console.log(memoized()); // Una única llamada // 3.1415
-//console.log(memoized()); // 3.1415
-//console.log(memoized()); // 3.1415
+const memoize = fn => {
+  let cache = {};
+  return (...args) => {
+    let stringifiedArgs = JSON.stringify(args);
+    let result = cache[stringifiedArgs] = cache[stringifiedArgs] || fn(...args);
+    return result;
+  }
+};
+
+const memoized = memoize(expensiveFunction);
+console.log(memoized()); // Una única llamada // 3.1415
+console.log(memoized()); // 3.1415
+console.log(memoized()); // 3.1415
